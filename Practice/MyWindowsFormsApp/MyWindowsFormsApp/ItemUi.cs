afd88b0b4fd96734b503a05e34fc80d887d0ccec
imranemoncse <imranemon6@gmail.com>
2019-10-15 17:21:27 +0600

    hello

---
+++ Practice/MyWindowsFormsApp/MyWindowsFormsApp/ItemUi.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyWindowsFormsApp
+{
+    public partial class ItemUi : Form
+    {
+        public ItemUi()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            //Check UNIQUE
+            if (IsNameExists(nameTextBox.Text))
+            {
+                MessageBox.Show(nameTextBox.Text + "Already Exists!");
+                return;
+            }
+
+            //Set Price as Mandatory
+            if (String.IsNullOrEmpty(priceTextBox.Text))
+            {
+                MessageBox.Show("Price Can not be Empty!!!");
+                return;
+            }
+
+            //Add/Insert Item
+            bool isAdded = Add(nameTextBox.Text, Convert.ToDouble(priceTextBox.Text));
+
+            if (isAdded)
+            {
+                MessageBox.Show("Saved");
+            }
+            else
+            {
+                MessageBox.Show("Not Saved");
+            }
+
+            Display();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            Display();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+
+            //Set Id as Mandatory
+            if (String.IsNullOrEmpty(idTextBox.Text))
+            {
+                MessageBox.Show("Id Can not be Empty!!!");
+                return;
+            }
+
+            //Delete
+            if (Delete(Convert.ToInt32(idTextBox.Text)))
+            {
+                MessageBox.Show("Deleted");
+            }
+            else
+            {
+                MessageBox.Show("Not Deleted");
+            }
+
+            Display();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            Search(nameTextBox.Text);
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            //Set Id as Mandatory
+            if (String.IsNullOrEmpty(idTextBox.Text))
+            {
+                MessageBox.Show("Id Can not be Empty!!!");
+                return;
+            }
+            //Set Price as Mandatory
+            if (String.IsNullOrEmpty(priceTextBox.Text))
+            {
+                MessageBox.Show("Price Can not be Empty!!!");
+                return;
+            }
+
+            if (Update(nameTextBox.Text, Convert.ToDouble(priceTextBox.Text), Convert.ToInt32(idTextBox.Text)))
+            {
+                MessageBox.Show("Updated");
+                Display();
+            }
+            else
+            {
+                MessageBox.Show("Not Updated");
+            }
+        }
+
+        //Method
+        //Method
+        private bool Add(string name, double price)
+        {
+            bool isAdded = false;
+            try
+            {
+                //Connection
+                string connectionString = @"Server=BITM-TRAINER-30\SQLEXPRESS; Database=CoffeeShop; Integrated Security=True";
+                SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+                //Command 
+                //INSERT INTO Items (Name, Price) Values ('Black', 120)
+                string commandString = @"INSERT INTO Items (Name, Price) Values ('" + name + "', " + price + ")";
+                SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+
+                //Open
+                sqlConnection.Open();
+                //Insert
+                int isExecuted = sqlCommand.ExecuteNonQuery();
+                if (isExecuted > 0)
+                {
+                    isAdded = true;
+                }
+
+                //if (!isNameExists(nameTextBox.Text))
+                //{
+                //    //Insert
+                //    int isExecuted = sqlCommand.ExecuteNonQuery();
+                //    if (isExecuted > 0)
+                //    {
+                //        isAdded = true;
+                //    }
+
+                //}
+                //else
+                //{
+                //    MessageBox.Show(nameTextBox.Text + "Already Exists!");
+                //}
+
+
+                //Close
+                sqlConnection.Close();
+
+
+            }
+            catch (Exception exeption)
+            {
+                MessageBox.Show(exeption.Message);
+            }
+
+            return isAdded;
+        }
+        private bool IsNameExists(string name)
+        {
+            bool exists = false;
+            try
+            {
+                //Connection
+                string connectionString = @"Server=BITM-TRAINER-30\SQLEXPRESS; Database=CoffeeShop; Integrated Security=True";
+                SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+                //Command 
+                //INSERT INTO Items (Name, Price) Values ('Black', 120)
+                string commandString = @"SELECT * FROM Items WHERE Name='" + name + "'";
+                SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+
+                //Open
+                sqlConnection.Open();
+                //Show
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+                if (dataTable.Rows.Count > 0)
+                {
+                    exists = true;
+                }
+                //Close
+                sqlConnection.Close();
+
+            }
+            catch (Exception exeption)
+            {
+                MessageBox.Show(exeption.Message);
+            }
+
+            return exists;
+        }
+        private void Display()
+        {
+            try
+            {
+                //Connection
+                string connectionString = @"Server=BITM-TRAINER-30\SQLEXPRESS; Database=CoffeeShop; Integrated Security=True";
+                SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+                //Command 
+                //INSERT INTO Items (Name, Price) Values ('Black', 120)
+                string commandString = @"SELECT * FROM Items";
+                SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+
+                //Open
+                sqlConnection.Open();
+
+                //Show
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+                if (dataTable.Rows.Count > 0)
+                {
+                    showDataGridView.DataSource = dataTable;
+                }
+                else
+                {
+                    MessageBox.Show("No Data Found");
+                }
+
+                //Close
+                sqlConnection.Close();
+
+            }
+            catch (Exception exeption)
+            {
+                MessageBox.Show(exeption.Message);
+            }
+        }
+        private bool Delete(int id)
+        {
+            try
+            {
+                //Connection
+                string connectionString = @"Server=BITM-TRAINER-30\SQLEXPRESS; Database=CoffeeShop; Integrated Security=True";
+                SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+                //Command 
+                //DELETE FROM Items WHERE ID = 3
+                string commandString = @"DELETE FROM Items WHERE ID = " + id + "";
+                SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+
+                //Open
+                sqlConnection.Open();
+
+                //Delete
+                int isExecuted = sqlCommand.ExecuteNonQuery();
+                if (isExecuted > 0)
+                {
+                    return true;
+                }
+
+
+                //Close
+                sqlConnection.Close();
+
+            }
+            catch (Exception exeption)
+            {
+                MessageBox.Show(exeption.Message);
+            }
+
+            return false;
+        }
+        private bool Update(string name, double price, int id)
+        {
+            try
+            {
+                //Connection
+                string connectionString = @"Server=BITM-TRAINER-30\SQLEXPRESS; Database=CoffeeShop; Integrated Security=True";
+                SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+                //Command 
+                //UPDATE Items SET Name =  'Hot' , Price = 130 WHERE ID = 1
+                string commandString = @"UPDATE Items SET Name =  '" + name + "' , Price = " + price + " WHERE ID = " + id + "";
+                SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+
+                //Open
+                sqlConnection.Open();
+
+                //Insert
+                int isExecuted = sqlCommand.ExecuteNonQuery();
+                if (isExecuted > 0)
+                {
+                    return true;
+                }
+                //Close
+                sqlConnection.Close();
+
+
+            }
+            catch (Exception exeption)
+            {
+                MessageBox.Show(exeption.Message);
+            }
+            return false;
+        }
+        private void Search(string name)
+        {
+            try
+            {
+                //Connection
+                string connectionString = @"Server=BITM-TRAINER-30\SQLEXPRESS; Database=CoffeeShop; Integrated Security=True";
+                SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+                //Command 
+                //INSERT INTO Items (Name, Price) Values ('Black', 120)
+                string commandString = @"SELECT * FROM Items WHERE Name='" + name + "'";
+                SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+
+                //Open
+                sqlConnection.Open();
+
+                //Show
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+                if (dataTable.Rows.Count > 0)
+                {
+                    showDataGridView.DataSource = dataTable;
+                }
+                else
+                {
+                    MessageBox.Show("No Data Found");
+                }
+
+                //Close
+                sqlConnection.Close();
+
+            }
+            catch (Exception exeption)
+            {
+                MessageBox.Show(exeption.Message);
+            }
+        }
+    }
+}

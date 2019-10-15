4e45d947e7ce89855e1bb66c265d5b1cd33982b6
imranemoncse <imranemon6@gmail.com>
2019-10-15 16:42:46 +0600

    my commit

---
+++ VechicleAppPractice2/Vechicle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VechicleAppPractice2
+{
+    class Vechicle
+    {
+        public string vechileName;
+        public string regNo;
+        public List<double> speed = new List<double>();
+
+       // public Vechicle(string vichcleName,string regNo )
+        //{
+           // this.vechileName = vichcleName;
+           // this.regNo = regNo;
+           
+       // }
+        public string VechileName { set; get; }
+        public string RegNo { set; get; }
+
+        public double GetMax()
+        {
+            double max = speed.Max();
+            return max;
+        }
+        public double GetMin()
+        {
+            double min = speed.Min();
+            return min;
+        }
+        public double GetAvg()
+        {
+            double avg = speed.Average();
+            return avg;
+        }
+
+    }
+}
---
+++ VechicleAppPractice2/VehicleUi.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VechicleAppPractice2
+{
+    public partial class VehicleUi : Form
+    {
+        public VehicleUi()
+        {
+            InitializeComponent();
+        }
+        Vechicle vechile = new Vechicle();
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            vechile.vechileName = vehicleNameTextBox.Text;
+            vechile.regNo = regNoTextBox.Text;
+            vehicleNameTextBox.Clear();
+            regNoTextBox.Clear();
+
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            maxSpeedTextBox.Text = vechile.GetMax().ToString();
+            minSpeedTextBox.Text = vechile.GetMin().ToString();
+            averageSpeedTextBox.Text = vechile.GetAvg().ToString();
+
+
+
+        }
+
+        private void minSpeedTextBox_TextChanged(object sender, EventArgs e)
+        {
+            vechile.GetMin();
+        }
+
+        private void maxSpeedTextBox_TextChanged(object sender, EventArgs e)
+        {
+            vechile.GetMax();
+        }
+
+        private void averageSpeedTextBox_TextChanged(object sender, EventArgs e)
+        {
+            vechile.GetAvg();
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            vechile.speed.Add(Convert.ToDouble(speedTextBox.Text));
+            speedTextBox.Clear();
+        }
+    }
+}
